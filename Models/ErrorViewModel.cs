c12d07e7d5c4debe3b501ff7ad26905adcad3d32
Scott Kirkland <srkirkland@gmail.com>
2020-10-27 10:36:06 -0700

    default netcore app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace codespaces_netcore3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
